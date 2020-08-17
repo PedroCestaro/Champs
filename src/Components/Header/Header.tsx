e4f30941c22fd015a22c0e60486f5efea8d1a4fe
Pedro Cestaro <pedrocmanoel@gmail.com>
2020-08-17 17:44:54 -0300

    Header Component finalizado

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import './styles.css';
+import Logo from '../../assets/log0.png';
+
 
 function Header() {
   return (
-      <div></div>
+      <div className="header">
+        <div className="logo">
+          <img src={Logo} alt="Champs-logo"/>
+        </div>
+      </div>
   );
 }      
 
