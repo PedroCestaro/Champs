70fb5a2d04f899f2330f1daea06d5be6adbb7d3d
Pedro Cestaro <pedrocmanoel@gmail.com>
2020-08-17 17:07:59 -0300

    iniciando projeto

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>Logo</header>
+      <div>
+        <h1>Campeonato</h1>
+
+        <h4>Results</h4>
+      </div>
+    </div>
+  );
+}
+
+export default App;
