f15269622667651bb65f3e1af2bd66468323d115
Pedro Cestaro <pedrocmanoel@gmail.com>
2020-08-20 17:19:09 -0300

    Landing page adaptada

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './Components/Header/Header';
 import Championship from './Components/Championship/Championship';
 import Landing from './pages/Landing/Landing';
-
+import './assets/styles/global.css';
 
 function App() {
 
